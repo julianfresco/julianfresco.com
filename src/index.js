181f26fb04fe160fe67ac43eec7fc7a6feb4b4b5
Julian Soro <contact@julianfresco.com>
2018-05-22 01:20:40 -0700

    task: Prepare for deploy to gh-pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import 'prismjs/themes/prism.css'
 import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+// registerServiceWorker()
